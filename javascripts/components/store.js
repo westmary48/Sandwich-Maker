a616e38a1cd5fb5bd72e8f4a83d56428befd24e9
Mary West <westmarye1219@gmail.com>
2019-04-13 10:36:08 -0500

    domString printing dynamically

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -6,16 +6,20 @@ import util from '../helpers/util.js';
     const breadInfo = bread.getBread();
     let domString = '<h2>Bread</h2>';
     domString += `<h3>${breadInfo.asiago.name}</h3>`
-    domString += `<h3>${breadInfo.asiago.price}</h3>`
+    domString += `<h3> $ ${breadInfo.asiago.price} .00 </h3>`
+    domString += `<img class = "breadImg" src = "${breadInfo.asiago.imageUrl}">`
     domString += `<button class = "btn btn-danger ${breadInfo.asiago.breadClassBtn} ">Add Asiago</button>`
     domString += `<h3>${breadInfo.wheat.name}</h3>`
-    domString += `<h3>${breadInfo.wheat.price}`
+    domString += `<h3> $ ${breadInfo.wheat.price} </h3>`
+    domString += `<img class = "breadImg" src = "${breadInfo.wheat.imageUrl}">`
     domString += `<button class = "btn btn-danger ${breadInfo.wheat.breadClassBtn} ">Add Wheat</button>`
     domString += `<h3>${breadInfo.white.name}</h3>`
-    domString += `<h3>${breadInfo.white.price}</h3>`
+    domString += `<h3> $ ${breadInfo.white.price}</h3>`
+    domString += `<img class = "breadImg" src = "${breadInfo.white.imageUrl}">`
     domString += `<button class = "btn btn-danger ${breadInfo.white.breadClassBtn} ">Add White</button>`
     domString += `<h3>${breadInfo.pita.name}</h3>`
-    domString += `<h3>${breadInfo.pita.price}</h3>`
+    domString += `<h3> $ ${breadInfo.pita.price}</h3>`
+    domString += `<img class = "breadImg" src= "${breadInfo.pita.imageUrl}">`
     domString += `<button class = "btn btn-danger ${breadInfo.pita.breadClassBtn} ">Add Pita</button>`
     domString += `</div>`
 
